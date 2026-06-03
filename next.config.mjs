8014fb9cf3c4260ab60ca8c7a312da1bdd20948f
aleemraza <aleemraza661@gmail.com>
2026-06-03 12:18:53 +0800

    Setup GitHub Pages deployment

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,18 @@
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   /* config options here */
+// };
+
+// export default nextConfig;
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  /* config options here */
+  output: 'export',
+  images: {
+    unoptimized: true,
+  },
+  basePath: '/ubicomp2026-quantum-tutorial',
+  assetPrefix: '/ubicomp2026-quantum-tutorial',
 };
 
 export default nextConfig;
